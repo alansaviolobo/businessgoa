4d5de226995fe45b375d691268611c462c23e689
Alan Lobo <alansaviolobo@gmail.com>
2010-05-06 07:49:57 +0000

    corrected the location of default logo

--- sites/all/themes/businessgoa/header_navigation.tpl.php
+++ sites/all/themes/businessgoa/header_navigation.tpl.php
@@ -12,7 +12,7 @@
 
 <div id="network">
 	<div class="center-wrapper">
-		<div class="left">	
+		<div class="left">
 			<ul class="tabbed" id="network-tabs">
 				<li class="current-tab"><a href="#">Business Goa</a></li>
 				<li><a href="#">Savoir Faire</a></li>
@@ -21,8 +21,8 @@
 		</div>
 		<div class="right">
 		<?php print $network_right; ?>
-			<div class="clearer">&nbsp;</div>		
-		</div>		
+			<div class="clearer">&nbsp;</div>
+		</div>
 		<div class="clearer">&nbsp;</div>
 	</div>
 </div>
@@ -35,13 +35,13 @@
 			</div>
 			<div class="clearer">&nbsp;</div>
 			<div id="site-title">
-				<h1><a href="#"><img src='<?php print $logo?>' width=500></a></h1>
+				<h1><a href="/"><img src='<?php print $logo?>'></a></h1>
 			</div>
 
-			<div id="navigation">				
+			<div id="navigation">
 				<div id="main-nav">
-					<?php 
-						//if (isset($primary_links)) print primary_links(); 
+					<?php
+						//if (isset($primary_links)) print primary_links();
 						print theme('links', $primary_links, array('class' => 'tabbed'));
 					?>
 					<div class="clearer">&nbsp;</div>
@@ -49,34 +49,34 @@
 
 				<div class="inline">
 					<div class="scroll-left"></div>
-					<div id="sub-nav">				
-									<?php 
-										//if (isset($secondary_links)) print secondary_links(); 
+					<div id="sub-nav">
+									<?php
+										//if (isset($secondary_links)) print secondary_links();
 										print theme('links', $secondary_links, array('class' => 'tabbed'));
-									?>				
+									?>
 					</div>
-					<div class="scroll-right"></div>	
+					<div class="scroll-right"></div>
 			</div>
 				<script type="text/javascript">
 					<!--
 					$(function(){
 						var div = $('#sub-nav');
-						
+
 						var current = 0;
 						var step = 2;
 						var maximum = $('#sub-nav ul li').length ;
 						var liWidth = $('#sub-nav ul li').outerWidth();
 						var carouselHeight = $('#sub-nav').outerHeight();
 						var speed = 200;
-						var visible = $('#sub-nav').outerWidth()/$('#sub-nav ul li').outerWidth();						
+						var visible = $('#sub-nav').outerWidth()/$('#sub-nav ul li').outerWidth();
 
 						div.css({overflow: 'hidden'});
-						
+
 						$('#sub-nav ul').css("position", "absolute");
-	 
+
 						$('#sub-nav').css("overflow", "hidden").css("position", "relative").css('height',"36px");
-						
-						$('.inline .scroll-right').click(function() { 
+
+						$('.inline .scroll-right').click(function() {
 							if(current + step < 0 || current - step > maximum - visible) {return; }
 							else {
 								current = current + step;
@@ -84,15 +84,15 @@
 							}
 							return false;
 						});
-				 
-						$('.inline .scroll-left').click(function() { 
+
+						$('.inline .scroll-left').click(function() {
 							if(current - step < 0 ) {return; }
 							else {
 								current = current - step;
 								$('#sub-nav ul').animate({left: -(liWidth * current)}, speed, null);
 							}
 							return false;
-						});					
+						});
 
 					});
 				-->
