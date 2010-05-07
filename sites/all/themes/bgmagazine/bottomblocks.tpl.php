168f78cc12d9568397fe7c57206ea03f9554ff80
Alan Lobo <alansaviolobo@gmail.com>
2010-05-07 13:16:00 +0000

    checking bg config module

---
+++ sites/all/themes/bgmagazine/bottomblocks.tpl.php
@@ -0,0 +1,22 @@
+<div class="content-separator"></div>
+<div class="col3 left">
+	<div class="column-content">
+		<div class="post">
+			<?= $bottom_left_region; ?>
+		</div>
+	</div>
+</div>
+<div class="col3 col3-mid left">
+	<div class="column-content">
+		<div class="post">
+			<?= $bottom_mid_region; ?>
+		</div>
+	</div>
+</div>
+<div class="col3 right">
+	<div class="column-content">
+		<div class="post">
+			<?= $bottom_right_region; ?>
+		</div>
+	</div>
+</div>
