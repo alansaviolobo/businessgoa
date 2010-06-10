719f2f082b302664b0ea637bee00492a3869492f
Alan Lobo <alansaviolobo@gmail.com>
2010-06-10 11:24:30 +0000

    corrected the content widths in the theme

--- sites/all/themes/bgmagazine/page-front.tpl.php
+++ sites/all/themes/bgmagazine/page-front.tpl.php
@@ -1,6 +1,5 @@
 <?php include("header_navigation.tpl.php");?>
 <div class="main" id="main-three-columns">
 <?php include("content.tpl.php");?>
-<?php include("bottomblocks.tpl.php");?>
 <?php include("sideblocks.tpl.php");?>
 <?php include("sidebar_footer.tpl.php");?>
