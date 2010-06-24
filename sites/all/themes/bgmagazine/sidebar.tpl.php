d071db276cd1511c827a7524dc5f4236c6f07e6d
Alan Lobo <alansaviolobo@gmail.com>
2010-06-24 18:34:59 +0000

    added new contact block, fixed blocks whitelist and added a advt title

--- sites/all/themes/bgmagazine/sidebar.tpl.php
+++ sites/all/themes/bgmagazine/sidebar.tpl.php
@@ -1,18 +1,25 @@
 <div class="right sidebar" id="sidebar-2">
 	<div class="section">
+		<div class="section-title text-centre">
+			<span class="smallest">Advertisement</span>
+		</div>
 		<div class="section-content">
 		<?= $ad_upper_region; ?>
 		</div>
 	</div>
 
 	<div class="section">
-		<div class="section-title"></div>
+		<div class="section-title">
+			<span class="smallest">Advertisement</span>
+		</div>
 		<div class="section-content">
 		<?= $ad_mid_region; ?>
 		</div>
 	</div>
 	<div class="section network-section">
-		<div class="section-title"></div>
+		<div class="section-title">
+			<span class="smallest">Advertisement</span>
+		</div>
 		<div class="section-content">
 		<?= $ad_lower_region; ?>
 		</div>
